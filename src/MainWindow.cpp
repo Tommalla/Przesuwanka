82c3dd444789a038ebdd4b0d9b75e5d4c44df1ba
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-04-28 19:08:58 +0200

    Poprawki w UI. Dodano okno nowej gry.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,14 +1,35 @@
+#include <QDesktopWidget>
+#include <QMessageBox>
+
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include "constants.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+	ui->setupUi(this);
+	
+	//podpinanie sygnałów
+	QObject::connect(ui->ActionZakoncz, SIGNAL(triggered()), this, SLOT(close()));
+	QObject::connect(ui->ActionAutor, SIGNAL(triggered()), this, SLOT(showHelp()));
+	QObject::connect(ui->ActionNowa, SIGNAL(triggered()), &newGameDialog, SLOT(exec()));
+	
+	//Wygląd - wyśrodkowanie, tytuł itd.
+	this->move(QApplication::desktop()->screen()->rect().center() - this->rect().center());
+	this->setWindowTitle(VERSION);
+	//TODO: Poprawić buttony i align na oknie nowej gry
+	this->newGameDialog.setWindowFlags(Qt::WindowCloseButtonHint);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+	delete ui;
 }
+
+void MainWindow::showHelp() {
+	QMessageBox msgBox;
+	msgBox.setWindowTitle("Autor");
+	msgBox.setText(VERSION + " by " + AUTHOR + "\n" + AUTHOR_MAIL + " 2013");
+	msgBox.exec();
+}
+
+
+
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -3,20 +3,27 @@
 
 #include <QMainWindow>
 
+#include "NewGameDialog.h"
+
 namespace Ui {
-class MainWindow;
+	class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT;
     
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	public:
+		explicit MainWindow(QWidget *parent = 0);
+		~MainWindow();
     
-private:
-    Ui::MainWindow *ui;
+	private:
+		Ui::MainWindow *ui;
+		NewGameDialog newGameDialog;
+		
+	private slots:
+		void showHelp();
+
 };
 
 #endif // MAINWINDOW_H
