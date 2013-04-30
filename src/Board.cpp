e3add7131853f405d0a74ec01b631d0af7c9a24c
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-04-30 22:38:02 +0200

    Klasa Board i poprawki w CMakeLists . Ukończona klasa Board bazująca na BitContainerze. Dodano odpowiednie klasy do src/CMakeLists.txt

--- src/Board.cpp
+++ src/Board.cpp
@@ -5,28 +5,40 @@ All rights reserved */
 
 #include "Board.h"
 
-void Board::setFieldAt (const Point& pos, const unsigned int field) {
+void Board::copyToSelf (const Board& b) {
+	this->size = b.size;
+	this->board = new BitContainer(*b.board);
+}
+
+
+void Board::setFieldAt (const Point& pos, const int field) {
 	assert(field < this->size * this->size);
 	assert(pos.x < this->size && pos.y < this->size);
 	
-	int row = (pos.y * this->size + pos.x) / this->fieldsPerRow;	//TUTAJ PEWNIE BĘDZIE DEBUG
-	int leftOver = (pos.y * this->size + pos.x) % this->fieldsPerRow;
-	
-	unsigned long long int mask = field;
-	mask <<= 64 - leftOver / this->bitsQty; 
-	
-	this->board[row] |= mask;
-	return;	//TODO sprawdź tę metodę dobrze!
+	this->board->setValue(this->size * pos.y + pos.x, field);
 }
 
 Board::Board (int size) {
 	this->size = size;
-	
-	this->bitsQty = ceil(2.0 * log2(size));
-	this->fieldsPerRow = 64 / bitsQty;
-	this->board.resize(ceil(double(size * size) / double(this->fieldsPerRow)));
+	this->board = new BitContainer(size * size, size);
 }
+
+Board::Board (const Board& b) {
+	this->copyToSelf(b);
+}
+
+Board::~Board() {
+	delete this->board;
+}
+
+Board& Board::operator= (const Board& b) {
+	this->copyToSelf(b);
+	return *this;
+}
+
 
 int Board::getFieldAt (const Point& pos) {
 	assert(pos.x < this->size && pos.y < this->size);
+	
+	return this->board->getValue(this->size * pos.y + pos.x);
 }
--- src/Board.h
+++ src/Board.h
@@ -1,25 +1,33 @@
 /* Tomasz [Tommalla] Zakrzewski, 2013
 All rights reserved */
-
 #ifndef BOARD_H
 #define BOARD_H
 #include <vector>
 
 #include "Point.h"
+#include "BitContainer.h"
 
 using namespace std;
 
 class Board {
 	private:
-		//bitsQty - ilość bitów na jedno pole
-		int size, bitsQty, fieldsPerRow;
-		vector<unsigned long long int> board;
+		int size;
+		BitContainer* board;
 		
-		void setFieldAt(const Point& pos, const unsigned int field);
-		//inline unsigned long long int getMask, int getRow
+		void copyToSelf(const Board &b);
+		void setFieldAt(const Point& pos, const int field);
 	public:
+		/**
+		 * @brief Tworzy nową planszę o boku podanej długości
+		 * @param size Długość boku planszy. Domyślnie 4.
+		 **/
 		Board(int size = 4);
+		Board(const Board &b);
+		
 		int getFieldAt(const Point& pos);
+		
+		~Board();
+		Board& operator=(const Board &b);
 };
 
 #endif // BOARD_H
