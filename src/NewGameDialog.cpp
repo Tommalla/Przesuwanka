4be8bc2f8ff9f6b59d16622e78f059afad6b19d5
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-03 23:16:19 +0200

    Poprawka warninga.

--- src/NewGameDialog.cpp
+++ src/NewGameDialog.cpp
@@ -6,14 +6,14 @@ NewGameDialog::NewGameDialog(QWidget *parent) : QDialog(parent), ui(new Ui::NewG
 }
 
 const GameType NewGameDialog::getGameType() {
-	if (ui->RandomRadioButton->isChecked())
-		return RANDOM;
 	if (ui->EasyRadioButton->isChecked())
 		return EASY;
 	if (ui->MediumRadioButton->isChecked())
 		return MEDIUM;
 	if (ui->HardRadioButton->isChecked())
 		return HARD;
+	
+	return RANDOM;
 }
 
 
