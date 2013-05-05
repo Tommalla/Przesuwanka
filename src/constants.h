ddb7ed34a6dbba3e7edb88fee6f7e887224dafc9
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-05 13:08:56 +0200

    BUGIFX Polonizacja

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 #include <QString>
+#include <string>
+
+using namespace std;
 
 const QString VERSION = "Przesuwanka v0.1";
 const QString AUTHOR = "Tomasz [Tommalla] Zakrzewski";
@@ -8,16 +11,18 @@ const QString AUTHOR_MAIL = "tz336079@students.mimuw.edu.pl";
 
 const QString AUTHOR_MESSAGE = "<center>" + VERSION + "<br/>" +AUTHOR + "<br/><a href=\"mailto:" + AUTHOR_MAIL + "\">" + AUTHOR_MAIL + 
 "</a><br/>IPP 2012/2013 <a href=\"http://www.mimuw.edu.pl\">MIMUW</a></center>";
-const QString HELP_MESSAGE = QString("<center> Ktoś był na tyle \"sprytny\", żeby wymyślić *genialną* nazwę \"przesuwanka\" na") +
+const QString HELP_MESSAGE = QString::fromUtf8((QString("<center> Ktoś był na tyle \"sprytny\", żeby wymyślić *genialną* nazwę \"przesuwanka\" na") +
 	QString("\"Grę w 15-tkę\" lub \"The game of 15\".<br/> Po krótce: Twoim zadaniem jest doprowadzić planszę do stanu, w ") +
 	QString("którym liczby będą kolejno uporządkowane wierszami.<br/>Więcej szczegółów: ") +
-	QString("<a href=\"http://pl.wikipedia.org/wiki/Piętnastka_(układanka)\">Wikipedia</a><br/></center>Klawiszologia:<br/>");
+	QString("<a href=\"http://pl.wikipedia.org/wiki/Piętnastka_(układanka)\">Wikipedia</a><br/></center>Klawiszologia:<br/>")).toStdString().c_str());
 	
-const QString SOLUTION_MSG = QString("Tryb pokazywania rozwiązania:<br/><b>Spacja</b> - Pauzuj/Kontynuuj Animację<br/>") +
+const QString SOLUTION_MSG = QString::fromUtf8(
+	(QString("Tryb pokazywania rozwiązania:<br/><b>Spacja</b> - Pauzuj/Kontynuuj Animację<br/>") +
 	QString("<b>1 x LPM</b> - Pazuj animację/kolejny krok animacji<br/>") +
 	QString("<b>1 x PPM</b> - Poprzedni krok pauzowanej animacji<br/>") +
 	QString("<b>2 x LPM</b> - Wznów animację<br/>") +
-	QString("<b>Strzałki lewo/prawo</b> - przewijanie pauzowanej animacji");
+	QString("<b>Strzałki lewo/prawo</b> - przewijanie pauzowanej animacji")).toStdString().c_str()
+	);
 	
 const int boardSize = 4;
 const int animationTime = 100;
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 
 int main(int argc, char** argv) {	
 	QApplication application(argc, argv);
+	
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+	
 	MainWindow mainWindow;
 	
 	mainWindow.show();
