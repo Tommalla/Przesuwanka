ca19e77849357b058c2476f6671ce9eb0e800532
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-05 11:40:25 +0200

    DEBUG Wyrzucono część irytujących debugów.

--- src/GraphicsTile.cpp
+++ src/GraphicsTile.cpp
@@ -74,25 +74,16 @@ void GraphicsTile::moveTile (const Point move) {
 
 
 void GraphicsTile::generatePixmap() {
-	qDebug("generatePixmap for number %d", number);
-	tileSize = min(parent->getScene()->width(), parent->getScene()->height());
-	tileSize /= parent->getSize();	//rozmiar sceny/ilość klocków
-	
-	qDebug("\tsceneSize: %i x %i => tileSize = %d\n\ttile: %d, %d, %d %d", parent->getScene()->width(), parent->getScene()->height(), 
-	       tileSize, 
-	       tileSize * this->relativePosition.x, tileSize * this->relativePosition.y,
-	       tileSize * (this->relativePosition.x + 1), tileSize * (this->relativePosition.y + 1) );
+	this->tileSize = min(parent->getScene()->width(), parent->getScene()->height());
+	this->tileSize /= parent->getSize();	//rozmiar sceny/ilość klocków
 	
 	QPixmap pixmap(tileSize, tileSize);
 	QPainter painter(&pixmap);
 	
-	qDebug("Color: %d %d %d", this->color.red(), this->color.green(), this->color.blue());
 	painter.fillRect(QRect(0, 0, tileSize, tileSize), this->color);
 	QString text = QString::number(number);
 	//TODO center!
 	painter.drawText(tileSize / 2 - 6, tileSize / 2 + 4, text);
-	
-	qDebug("\tText: %s", text.toStdString().c_str());
 	
 	this->setPixmap(pixmap);
 	this->setOffset(this->relativePosition.x * tileSize, this->relativePosition.y * tileSize);
@@ -113,7 +104,6 @@ GraphicsTile::GraphicsTile (GameHandler* parent, const int number, const Point r
 	int r = rand() % 256;
 	int g = rand() % 256;
 	int b = rand() % 256;
-	qDebug("\tColor: %d %d %d", r, g, b);
 	this->color = QColor(r, g, b);
 	
 	this->generatePixmap();
